a7bc0f0d3ebcbbb5306f01b053685ef34dce74bb
ndk4696 <dineskn4696@gmail.com>
2025-04-01 12:29:38 -0400

    updated size, animation, scroll wrapper, and layout tweaks

--- script.js
+++ script.js
@@ -1,87 +1,136 @@
-//navi bar tab
+// --- Sticky Navigation ---
 class StickyNavigation {
-	
 	constructor() {
-		this.currentId = null;
-		this.currentTab = null;
-		this.tabContainerHeight = 70;
-		let self = this;
-		$('.tab').click(function() { 
-			self.onTabClick(event, $(this)); 
-		});
-		$(window).scroll(() => { this.onScroll(); });
-		$(window).resize(() => { this.onResize(); });
+	  this.currentId = null;
+	  this.currentTab = null;
+	  this.tabContainerHeight = 70;
+	  let self = this;
+	  $('.tab').click(function () {
+		self.onTabClick(event, $(this));
+	  });
+	  $(window).scroll(() => this.onScroll());
+	  $(window).resize(() => this.onResize());
 	}
-	
+  
 	onTabClick(event, element) {
-		event.preventDefault();
-		let scrollTop = $(element.attr('href')).offset().top - this.tabContainerHeight + 1;
-		$('html, body').animate({ scrollTop: scrollTop }, 600);
+	  event.preventDefault();
+	  let scrollTop = $(element.attr('href')).offset().top - this.tabContainerHeight + 1;
+	  $('html, body').animate({ scrollTop: scrollTop }, 600);
 	}
-	
+  
 	onScroll() {
-		this.checkTabContainerPosition();
-    this.findCurrentTabSelector();
+	  this.checkTabContainerPosition();
+	  this.findCurrentTabSelector();
 	}
-	
+  
 	onResize() {
-		if(this.currentId) {
-			this.setSliderCss();
-		}
+	  if (this.currentId) {
+		this.setSliderCss();
+	  }
 	}
-	
+  
 	checkTabContainerPosition() {
-		let offset = $('.tabs').offset().top + $('.tabs').height() - this.tabContainerHeight;
-		if($(window).scrollTop() > offset) {
-			$('.tabs-container').addClass('tabs-container--top');
-		} 
-		else {
-			$('.tabs-container').removeClass('tabs-container--top');
-		}
+	  let offset = $('.tabs').offset().top + $('.tabs').height() - this.tabContainerHeight;
+	  if ($(window).scrollTop() > offset) {
+		$('.tabs-container').addClass('tabs-container--top');
+	  } else {
+		$('.tabs-container').removeClass('tabs-container--top');
+	  }
 	}
-	
-	findCurrentTabSelector(element) {
-		let newCurrentId;
-		let newCurrentTab;
-		let self = this;
-		$('.tab').each(function() {
-			let id = $(this).attr('href');
-			let offsetTop = $(id).offset().top - self.tabContainerHeight;
-			let offsetBottom = $(id).offset().top + $(id).height() - self.tabContainerHeight;
-			if($(window).scrollTop() > offsetTop && $(window).scrollTop() < offsetBottom) {
-				newCurrentId = id;
-				newCurrentTab = $(this);
-			}
-		});
-		if(this.currentId != newCurrentId || this.currentId === null) {
-			this.currentId = newCurrentId;
-			this.currentTab = newCurrentTab;
-			this.setSliderCss();
+  
+	findCurrentTabSelector() {
+	  let newCurrentId;
+	  let newCurrentTab;
+	  let self = this;
+	  $('.tab').each(function () {
+		let id = $(this).attr('href');
+		let offsetTop = $(id).offset().top - self.tabContainerHeight;
+		let offsetBottom = $(id).offset().top + $(id).height() - self.tabContainerHeight;
+		if ($(window).scrollTop() > offsetTop && $(window).scrollTop() < offsetBottom) {
+		  newCurrentId = id;
+		  newCurrentTab = $(this);
 		}
+	  });
+	  if (this.currentId !== newCurrentId || this.currentId === null) {
+		this.currentId = newCurrentId;
+		this.currentTab = newCurrentTab;
+		this.setSliderCss();
+	  }
 	}
-	
+  
 	setSliderCss() {
-		let width = 0;
-		let left = 0;
-		if(this.currentTab) {
-			width = this.currentTab.css('width');
-			left = this.currentTab.offset().left;
-		}
-		$('.tab-slider').css('width', width);
-		$('.tab-slider').css('left', left);
+	  if (this.currentTab) {
+		$('.tab-slider').css({
+		  width: this.currentTab.css('width'),
+		  left: this.currentTab.offset().left
+		});
+	  }
 	}
-	
-}
-
-new StickyNavigation();
-
-// Remove blur after frame 3
-setTimeout(() => {
-	document.getElementById('content-wrapper').classList.add('deblur');
-}, 3000);
-
-// Show scroll animation after Frame 4 finishes
-document.getElementById('animation').addEventListener('animationend', () => {
-	document.getElementById('scroll-container').style.opacity = 1;
-});
-
+  }
+  
+  new StickyNavigation();
+  
+  
+  // --- Flicker-Free Image Animation ---
+  const framePaths = [
+	"/image/Frame1.png",
+	"/image/Frame2.png",
+	"/image/Frame3.png",
+	"/image/Frame4.png"
+  ];
+  
+  const preloadImages = (paths, callback) => {
+	let loaded = 0;
+	const total = paths.length;
+  
+	paths.forEach((path) => {
+	  const img = new Image();
+	  img.onload = () => {
+		loaded++;
+		if (loaded === total) callback();
+	  };
+	  img.onerror = () => {
+		loaded++;
+		if (loaded === total) callback();
+	  };
+	  img.src = path;
+	});
+  };
+  
+  const playAnimation = () => {
+	const imgEl = document.getElementById("animation");
+	const contentWrapper = document.getElementById("content-wrapper");
+	const scrollWrapper = document.getElementById("scroll-container");
+  
+	imgEl.style.opacity = 1;
+  
+	let frame = 0;
+	const totalFrames = framePaths.length;
+	const frameDuration = 800; // 6 seconds total
+  
+	const interval = setInterval(() => {
+	  frame++;
+  
+	  // Change image frame
+	  if (frame < totalFrames) {
+		imgEl.src = framePaths[frame];
+	  }
+  
+	  // Deblur after frame 2 (index 2)
+	  if (frame === 2) {
+		contentWrapper.classList.add('deblur');
+	  }
+  
+	  // Show scroll after frame 3 (index 3)
+	  if (frame === 3) {
+		scrollWrapper.classList.add('visible'); // triggers fade-in
+	  }
+  
+	  if (frame >= totalFrames) {
+		clearInterval(interval);
+	  }
+	}, frameDuration);
+  };
+  
+  preloadImages(framePaths, playAnimation);
+  
